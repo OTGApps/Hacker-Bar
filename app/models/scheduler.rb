0b137f7382d6e039617425bf45c6e2f0d878c5f6
Mark Rickert <mjar81@gmail.com>
2013-11-04 16:37:22 -0500

    Move over to bubble wrap timers instead of manual.

--- app/models/scheduler.rb
+++ app/models/scheduler.rb
@@ -12,20 +12,33 @@ class Scheduler
 		failsafe
   end
 
+  def refresh_and_start_polling
+    App.delegate.refresh
+    start_polling
+  end
+
   def start_polling
     ap "Starting to poll"
     NSLog "Starting to poll"
-  	if @timer.nil? || !@timer.isValid
+
+    EM.cancel_timer(@timer) if @timer
+
+    if @timer.nil?
       interval = App::Persistence['check_interval'];
       ap "Wait #{interval} seconds"
-      @timer = NSTimer.scheduledTimerWithTimeInterval(interval, target:self, selector:"go_go_gadget_scheduler", userInfo:nil, repeats:true)
+
+      @timer = EM.add_periodic_timer interval do
+        ap "Refreshing at #{Time.now}"
+        App.delegate.refresh
+      end
   	end
   end
 
   def stop_polling
     ap "Stopping the polling"
     NSLog "Stopping the polling"
-    @timer.invalidate
+    EM.cancel_timer(@timer)
+    @timer = nil
   end
 
   def restart_polling
@@ -33,15 +46,11 @@ class Scheduler
   	start_polling
   end
 
-  def go_go_gadget_scheduler
-  	ap "Refreshing"
-    App.delegate.refresh
-  end
-
   def failsafe
-  	# Don't hammer our server please.
-  	if App::Persistence['check_interval'] < 60
-  		App::Persistence['check_interval'] = 60
+  	interval = App::Persistence['check_interval']
+    # Don't hammer our server please.
+  	if interval > 0 && interval < 60
+  		interval = 60
 	    alert = NSAlert.alloc.init
 	    alert.setMessageText "Please don't try and fetch data from our server more often than every 60 seconds.\n\nThanks!"
 	    alert.addButtonWithTitle "OK, I'm sorry"
