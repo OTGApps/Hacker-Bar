cf8704f07c762396eda095a192d6e506734a818b
Mark Rickert <mjar81@gmail.com>
2013-11-05 19:12:37 -0500

    Apparently there needs to be a backslash here.

--- HackerBarLauncher/app/app_delegate.rb
+++ HackerBarLauncher/app/app_delegate.rb
@@ -1,6 +1,6 @@
 class AppDelegate
   def applicationDidFinishLaunching(notification)
-    app_name = "Hacker Bar"
+    app_name = "Hacker\ Bar"
     if NSWorkspace.sharedWorkspace.launchApplication(app_name)
       NSApp.performSelector("terminate:", withObject:nil, afterDelay: 0.0)
     else
