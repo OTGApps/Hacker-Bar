229623c3e7953148bae3c03fd29e1c8667701c34
Mark Rickert <mjar81@gmail.com>
2013-11-10 22:56:53 -0500

    Change failsafe to 2 minutes instead of one.

--- app/models/scheduler.rb
+++ app/models/scheduler.rb
@@ -40,12 +40,12 @@ class Scheduler
 
   def failsafe
     # Don't hammer our server please.
-    if App::Persistence['check_interval'].to_i < 60
-      App::Persistence['check_interval'].to_i = 60
+    if App::Persistence['check_interval'].to_i < 120
+      App::Persistence['check_interval'].to_i = 120
 
       EM.schedule do
         alert = NSAlert.alloc.init
-        alert.setMessageText "Please don't try and fetch data from our server more often than every 60 seconds.\n\nThanks!"
+        alert.setMessageText "Please don't try and fetch data from our server more often than every 2 minutes.\n\nThanks!"
         alert.addButtonWithTitle "OK, I'm sorry"
         alert.runModal
       end
