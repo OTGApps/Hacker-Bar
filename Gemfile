1ce0e9a7ab2101e10364d4c664725c19a665a081
Mark Rickert <mjar81@gmail.com>
2014-01-30 18:27:10 -0500

    Mmograte from BW::HTTP to AFMotion
    HT: @jamonholmgren
    http://jamonholmgren.com/refactoring-from-bw-http-to-afmotion

--- Gemfile
+++ Gemfile
@@ -7,6 +7,6 @@ gem "cocoapods"
 gem 'rake'
 
 gem 'ib'
-gem 'bubble-wrap', '1.4.0', github: 'rubymotion/BubbleWrap', require: ['bubble-wrap/core', 'bubble-wrap/http', 'bubble-wrap/reactor']
-
+gem 'bubble-wrap', '1.4.0', github: 'rubymotion/BubbleWrap', require: ['bubble-wrap/core', 'bubble-wrap/reactor']
+gem "afmotion", "~> 2.0.0"
 gem 'awesome_print_motion'
--- app/helpers/hn_api.rb
+++ app/helpers/hn_api.rb
@@ -1,35 +1,31 @@
 class HNAPI
 
-  # APIURL = "http://localhost:3000/hn/"
-  APIURL = "http://www.hackerbarapp.com/api/"
+  # API_URL = "http://localhost:3000/hn/"
+  API_URL = "http://www.hackerbarapp.com/"
+  API_KEY = "2659a3fd-2f13-40ed-86ca-b7a40692979c"
 
   def self.get_news(&block)
 
-    params = {
-      api_key: "2659a3fd-2f13-40ed-86ca-b7a40692979c"
-    }
-
-    headers = {
-      accept: "application/com.mohawkapps.hacker-bar; version=1"
-    }
-
-    options = {
-      payload: params,
-      headers: headers,
-    }
-
-    BW::HTTP.get(APIURL, options) do |response|
-        json = nil
-        error = nil
-
-        if response.ok?
-          json = BW::JSON.parse(response.body.to_str)
-        else
-          error = BW::JSON.parse(response.body.to_str)
-        end
-
-        block.call json, error
+    client = AFMotion::Client.build(API_URL) do
+      header "Accept", "application/com.mohawkapps.hacker-bar; version=1"
+      response_serializer :json
     end
+
+    client.get("api", api_key:API_KEY) do |result|
+      json = nil
+      error = nil
+
+      ap result
+
+      if result.success?
+        json = result.object
+      else
+        error = result
+      end
+
+      block.call json, error
+    end
+
   end
 
 end
